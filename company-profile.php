596f5b7cde866382b9c590e58a045da453c0b165
nabilajui <nabilajui5681@gmail.com>
2017-01-29 17:21:17 +0600

    nabila 5.21

---
+++ company-profile.php
@@ -0,0 +1,68 @@
+<?php
+$content = '<div class="main-content">
+      
+      <div class="content-left">
+        <h3>Company Profile</h3>
+        <div class="content-details">
+          <div id="scrollbar2">
+              <div class="scrollbar" style="height: 310px;"><div class="track" style="height: 310px;"><div class="thumb" style="top: 26.1603px; height: 101.371px;"><div class="end"></div></div></div></div>
+              <div class="viewport">
+                  <div class="overview" style="top: -80px;">
+                                        <p>ANGAN Properties Ltd. (A sister concern of SALMA GROUP) started its journey in 2008,Though The Group started its journey in 2000 and has since become one of the forerunners in Spinning Sector , earning a great reputation.<br><br>
+
+SALMA Group consists with SALMA TRADE INT. BSB Spinning Mills, DABIRUDDIN Spinning Mills, SURAIYA Spinning Mills, TAMIJ UDDIN Spinning Mills Ltd, MOSHIUR Traders, Crystal Bridge (Pvt.) Ltd. M/S. Bhuiyan Enterprise.<br><br>
+
+ANGAN PROPERTIES LTD. (Angan Properties LTD) was formed under the Chairmanship of Mr. Chowdhury Mohammed Hanif to cater the boom in Real Estate Development in Bangladesh. Angan Properties LTD within a short span of time turned out as one of the most trusted company in this sector and has already been working in various numbers of apartments projects. The motto of Angan Properties LTD is not only to deliver apartment of schedule but also to maintain the highest-grade construction quality that has gained trust and respect in customers.<br><br>
+
+Eventually in 2008, believing that construction is not just about building structures - but an Art, and with a challenge to go beyond the traditional scopes of property development, ANGAN PROPERTIES LTD. (Angan Properties LTD) was established with a mission to change the lifestyle of city dwellers by providing modern, functional and aesthetic living and working spaces that can only be compared to the most successful developers of the globe. Since then Angan Properties LTD has emerged as the most reputed and fastest growing real estate developer of the country.<br><br>
+
+VISION: <br>
+To set Angan Properties LTD as an icon for both clients and competitors in the fields of innovation, product quality, service standard, profitability and community work.
+
+MISSION: <br>
+For Our Clients: To offer a global touch, maintaining Bangladeshi culture in living and work places, with utmost uncompromising service to our clients and value for money.
+<br><br>
+For Our Investors: To ensure a smooth upward-reasonable trend of return on investment.
+<br><br>
+For Our Employees: To give employees a feeling of satisfaction by maximizing their potentials and providing means for their personal well-being and career development.
+<br><br>
+OUR APPROACH: 
+Acquiring an apartment, a home or even an office space is a person\'s life-long dream. This dream drives him or her to accumulate the required finance slowly and gradually, which is the start of shaping this dream into reality. This relentless pursuit of realizing such a dream can flow from generation to generation. But finally when a space is purchased, has anyone thought to what extent this “dream” is actually fulfilled?
+<br><br>
+We know that moment is an important stepping-stone for you, and hence we at ANGAN Properties Ltd. have been preparing for just that. Our apartments and commercial complex compete with the best that the modern world has to offer. From temperature-controlled swimming pools to lush rooftop gardens and terraces, from state-of-the-art gymnasiums to spacious walkways and children’s play facilities, you’ll find the solution to your need for stylish urban living with us at ANGAN. Even business becomes pleasure in our exquisite commercial spaces with central air-conditioning, triple height lobby areas, multilayered parking facilities, and much more. Show the world you’ve arrived, by owning your own little piece of luxury.
+</p>
+                    <!--<p>ANGAN Properties Ltd. (A sister concern of SALMA GROUP) started its journey in 2008,Though The Group started its journey in 2000 and has since become one of the forerunners in Spinning  Sector , earning a great reputation.</p>
+
+                    <p>SALMA Group consists with SALMA TRADE INT. BSB Spinning Mills, DABIRUDDIN Spinning Mills, SURAIYA Spinning Mills, TAMIJ UDDIN Spinning Mills Ltd, MOSHIUR Traders, Crystal Bridge (Pvt.) Ltd. M/S. Bhuiyan Enterprise.</p>
+
+                    <p>ANGAN PROPERTIES LTD. (Angan Properties LTD) was formed under the Chairmanship of  Mr. Chowdhury Mohammed Hanif to cater the boom in Real Estate Development in Bangladesh.
+                    Angan Properties LTD within a short span of time turned out as one of the most trusted company in this sector and has already been working in various numbers of apartments projects. The motto of Angan Properties LTD is not only to deliver apartment of schedule but also to maintain the highest-grade construction quality that has gained trust and respect in customers.</p>
+
+                    <p>Eventually in 2008, believing that construction is not just about building structures - but an Art, and with a challenge to go beyond the traditional scopes of property development, ANGAN PROPERTIES LTD. (Angan Properties LTD) was established with a mission to change the lifestyle of city dwellers by providing modern, functional and aesthetic living and working spaces that can only be compared to the most successful developers of the globe.  Since then Angan Properties LTD has emerged as the most reputed and fastest growing real estate developer of the country.</p>
+
+                    <p><strong>VISION:</strong> <br>
+                    To set Angan Properties LTD as an icon for both clients and competitors in the fields of innovation, product quality, service standard, profitability and community work.</p>
+
+                    <p><strong>MISSION:</strong> <br> 
+                    For Our Clients: To offer a global touch, maintaining Bangladeshi culture in living and work places, with utmost uncompromising service to our clients and value for money.</p>
+                     
+                    <p>For Our Investors: To ensure a smooth upward-reasonable trend of return on investment.</p>
+                     
+                    <p>For Our Employees: To give employees a feeling of satisfaction by maximizing their potentials and providing means for their personal well-being and career development.</p>
+
+                    <p><strong>OUR APPROACH:</strong> <br>
+                    Acquiring an apartment, a home or even an office space is a person\'s life-long dream. This dream drives him or her to accumulate the required finance slowly and gradually, which is the start of shaping this dream into reality. This relentless pursuit of realizing such a dream can flow from generation to generation. But finally when a space is purchased, has anyone thought to what extent this “dream” is actually fulfilled?</p>
+                     
+                    <p>We know that moment is an important stepping-stone for you, and hence we at ANGAN  Properties Ltd. have been preparing for just that. Our apartments and commercial complex compete with the best that the modern world has to offer. From temperature-controlled swimming pools to lush rooftop gardens and terraces, from state-of-the-art gymnasiums to spacious walkways and children’s play facilities, you’ll find the solution to your need for stylish urban living with us at ANGAN. Even business becomes pleasure in our exquisite commercial spaces with central air-conditioning, triple height lobby areas, multilayered parking facilities, and much more. Show the world you’ve arrived, by owning your own little piece of luxury.</p>-->
+                    </div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="content-right">
+          
+          <img src="http://anganpropertiesltd.com/wp-content/uploads/2017/01/company-profile.png">
+      </div>
+    </div>';
+include('index.php')
+?>
---
+++ projects.php
@@ -0,0 +1,34 @@
+<?php
+$content = '<div class="main-content projects-area">
+      
+      <div class="content-left projects-area-projects">
+        <h3>Projects</h3>
+        <div class="content-details projects-area-projects-details">
+          <div class="image image-projects">
+            <a href="/angan/ongoing-projects.php">
+                                <img src="http://anganpropertiesltd.com/wp-content/uploads/2017/01/ongoing.jpg" alt="image">
+                <span class="span-projects">Ongoing<br> Projects</span>
+            </a>
+          </div>
+
+          <div class="image image-projects">
+            <a href="/angan/completed-projects.php">
+                <img src="http://anganpropertiesltd.com/wp-content/themes/anjon-properties/assets/images/completed.jpg" alt="">
+              <span class="span-projects">Completed<br> Projects</span>
+            </a>
+          </div>
+
+          <div class="image image-projects">
+            <a href="/angan/upcoming-projects.php">
+                <img src="http://anganpropertiesltd.com/wp-content/themes/anjon-properties/assets/images/upcoming.jpg" alt="">
+              <span class="span-projects">Upcoming <br>Projects</span>
+            </a>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>';
+include('index.php');
+?>
