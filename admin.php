c3bd1e7bb15eff80b63e924f63849a568c83dedf
mahbubmoon <mahbub.moon6@gmail.com>
2017-01-30 14:39:03 +0600

    mahbub admin page design 2.38

--- admin.php
+++ admin.php
@@ -1,22 +1,26 @@
 <?php
    $content = '<form action="submitProject.php" method="post" enctype="multipart/form-data">
-    <div class="form-group">
-      <label for="email">Project Name:</label>
-      <input type="text" class="form-control" id="projectName" name="projectName" placeholder="Project Name">
-    </div>
-    <div class="form-group">
-      <label for="email">Upload Image</label>
-      <input type="file" class="form-control" id="image" name="image[]" multiple accept="image/x-png,image/gif,image/jpeg">
-    </div>
-    <div class="form-group">
-      <label for="email">Location</label>
-      <input type="text" class="form-control" id="location" name="location" placeholder="Location">
-    </div>
-    <div class="form-group">
-      <label for="email">Details:</label>
-      <textarea type="text" class="form-control" id="details" name="details"></textarea>
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
+   <ul class="form-style-1">
+    <li>
+      <label for="email"><span class="required">*</span>Project Name:</label>
+      <input type="text" class="field-long" id="projectName" name="projectName" placeholder="Project Name">
+    </li>
+    <li>
+      <label for="email"><span class="required">*</span>Upload Image</label>
+      <input type="file" class="field-long" id="image" name="image[]" multiple accept="image/x-png,image/gif,image/jpeg">
+   </li>
+    <li>
+      <label for="email"><span class="required">*</span>Location</label>
+      <input type="text" class="field-long" id="location" name="location" placeholder="Location">
+    </li>
+    <li>
+    <label for="details"><span class="required">*</span>Project Details</label>
+      <textarea type="text" rows="8" class="field-long" id="details" name="details"></textarea>
+    </li>
+    <li>
+    <input type="submit" class="btn btn-default"/>
+    </li>
+    </ul>
   </form>
 ';
    include('index.php');
--- index.php
+++ index.php
@@ -47,6 +47,108 @@ img.emoji {
 	background: none !important;
 	padding: 0 !important;
 }
+.form-style-1 {
+
+    max-width: 400px;
+    padding: 20px 12px 10px 20px;
+    font: 13px "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+}
+.form-style-1 li {
+    padding: 0;
+    display: block;
+    list-style: none;
+    margin: 10px 0 0 0;
+}
+.form-style-1 label{
+    margin:0 0 3px 0;
+    padding:0px;
+    display:block;
+    font-weight: bold;
+}
+.form-style-1 input[type=text], 
+.form-style-1 input[type=date],
+.form-style-1 input[type=datetime],
+.form-style-1 input[type=number],
+.form-style-1 input[type=search],
+.form-style-1 input[type=time],
+.form-style-1 input[type=url],
+.form-style-1 input[type=email],
+textarea, 
+select{
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    border:1px solid #BEBEBE;
+    padding: 7px;
+    margin:0px;
+    -webkit-transition: all 0.30s ease-in-out;
+    -moz-transition: all 0.30s ease-in-out;
+    -ms-transition: all 0.30s ease-in-out;
+    -o-transition: all 0.30s ease-in-out;
+    outline: none;  
+}
+.form-style-1 input[type=text]:focus, 
+.form-style-1 input[type=date]:focus,
+.form-style-1 input[type=datetime]:focus,
+.form-style-1 input[type=number]:focus,
+.form-style-1 input[type=search]:focus,
+.form-style-1 input[type=time]:focus,
+.form-style-1 input[type=url]:focus,
+.form-style-1 input[type=email]:focus,
+.form-style-1 textarea:focus, 
+.form-style-1 select:focus{
+    -moz-box-shadow: 0 0 8px #88D5E9;
+    -webkit-box-shadow: 0 0 8px #88D5E9;
+    box-shadow: 0 0 8px #88D5E9;
+    border: 1px solid #88D5E9;
+}
+.form-style-1 .field-divided{
+    width: 49%;
+}
+
+.form-style-1 .field-long{
+    width: 100%;
+}
+.form-style-1 .field-select{
+    width: 100%;
+}
+.form-style-1 .field-textarea{
+    height: 100px;
+}
+.form-style-1 input[type=submit], .form-style-1 input[type=button]{
+    background: #4B99AD;
+    padding: 8px 15px 8px 15px;
+    border: none;
+    color: #fff;
+}
+.form-style-1 input[type=submit]:hover, .form-style-1 input[type=button]:hover{
+    background: #4691A4;
+    box-shadow:none;
+    -moz-box-shadow:none;
+    -webkit-box-shadow:none;
+}
+.form-style-1 .required{
+    color:red;
+}
+.admin{
+    
+    margin-bottom:20px;
+    margin-top: 8px;
+}
+a.admin-menu{
+    background-color:rgb(181,15,25);
+color: #fff !important;
+font-weight: bold;
+font-family: 'Exo 2', Verdana, Geneva, sans-serif;
+font-size: 18px;
+min-width: 200px !important;
+padding: 8px;
+margin: 0px 0px;
+}
+a.admin-menu:hover{
+    background-color: #9B0F16;
+    text-decoration: none !important;
+}
 img{
     width: 100%;
     height: auto;
@@ -96,7 +198,17 @@ img{
     <div class="main-content">
         <div class="home-slider">
            <?php 
-           if(isset($content))echo $content; 
+           if(isset($content)){
+               $admin = '<div class="admin"><a class="admin-menu" href="/angan/admin.php">New Project</a>
+                         <a class="admin-menu" href="/angan/admin_up_coming.php">Upcoming Projects</a>
+                         <a class="admin-menu" href="/angan/admin_on_going.php">Ongoing Projects</a>
+                         <a class="admin-menu" href="/angan/admin_completed.php">Completed Projects</a></div>';
+               if(strpos($_SERVER['REQUEST_URI'],'admin')){
+                    echo $admin;
+               }
+               echo $content;
+        
+           }
            else {
                $content = '
 <div id="sliderFrame">
