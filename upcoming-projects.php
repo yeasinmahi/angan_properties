951e572ea0370d3184251a8ba1e3c425feeea3fa
mahbubmoon <mahbub.moon6@gmail.com>
2017-01-30 16:04:52 +0600

    mahbub admin-up-coming page design 4.04

--- upcoming-projects.php
+++ upcoming-projects.php
@@ -1,4 +1,14 @@
 <?php
+require 'dbConnector.php';
+$sql = "SELECT * FROM `projects` as p join image as i on p.id = i.pid where status = 1";
+$result = $conn->query($sql);
 $content = '';
+if ($result->num_rows > 0) { 
+    while($row = $result->fetch_assoc()) {
+        $content .= '<a href="#" class="project-gallery"><img  src="/angan/'.$row["image"].'"></img></a></td>
+        ';
+    }
+} 
+
 include('index.php');
 ?>
