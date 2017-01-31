7bb7d4bfbe9571b070aa799fb46d53dc4b5ea6ce
mahbubmoon <mahbub.moon6@gmail.com>
2017-01-31 11:09:27 +0600

    mahbub slide images

--- dbConnector.php
+++ dbConnector.php
@@ -1,15 +1,9 @@
 <?php
 
-// $servername = "103.203.95.42:3306";
-// $username = "angan";
-// $password = "1234";
-// $dbname = "angandb";
-
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "103.203.95.42:3306";
+$username = "angan";
+$password = "1234";
 $dbname = "angandb";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
